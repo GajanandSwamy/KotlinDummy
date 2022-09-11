33b42418a8cde33761e764a2e14a35595eb23ca3
Gaju <gajanad2013@gmail.com>
2022-09-11 13:41:41 +0530

    changes in test file

--- Test1.kt
+++ Test1.kt
@@ -3,12 +3,14 @@ package com.example.gajanand.kotlindummy
 fun main() {
 gi()
 }
-fun gi(){
-    for (i in 1..4)
+fun gi() {
+    for (i in 1..4) {
         print(i)
-    if (7 in 1..6)
-        print("yes")
+        print(i)
+        if (7 in 1..6)
+            print("yes")
 
-    for (j in 5..6)
-        print(j)
+        for (j in 5..6)
+            print(j)
+    }
 }
